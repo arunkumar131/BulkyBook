fc887fe8fb9fa8c531321c043b47fffa019d8c5b
Arun Kumar <arun.kumar@klm.com>
2020-02-27 14:27:20 +0100

    Add Cover Type Model and Repository Setup

---
+++ BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System.Linq;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(c => c.Id == coverType.Id);
+           if(objFromDb !=null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -17,6 +17,8 @@ namespace BulkyBook.DataAccess.Repository
 
         public ICategoryRepository Category { get; private set; }
 
+        public ICoverTypeRepository CoverType { get; private set; }
+
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
