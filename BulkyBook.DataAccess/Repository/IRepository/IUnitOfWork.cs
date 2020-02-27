cb81fa197148b31cecf04ff027f1c42abc20bacf
Arun Kumar <arun.kumar@klm.com>
2020-02-27 10:22:25 +0100

    Add Category Controller and GetAll Action

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -2,7 +2,7 @@
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork :IDisposable
+    public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
         ISP_Call SP_Call { get; }
