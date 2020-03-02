aadb8354a41520cdc556f97ad5fbbb4d0caba731
Arun Kumar <arun.kumar@klm.com>
2020-03-02 11:22:48 +0100

    Add Product Repository

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -5,21 +5,35 @@ using System.Linq;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class ProductRepository : Repository<Product>, IProductRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db) : base(db)
+        public ProductRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
 
-        public void Update(Category category)
+        public void Update(Product product)
         {
-            var objFromDb = _db.Categories.FirstOrDefault(c => c.Id == category.Id);
+            var objFromDb = _db.Products.FirstOrDefault(c => c.Id == product.Id);
             if(objFromDb != null)
             {
-                objFromDb.Name = category.Name;
+                if(product.ImageUrl !=null)
+                {
+                    objFromDb.ImageUrl = product.ImageUrl;
+                }
+
+                objFromDb.ISBN = product.ISBN;
+                objFromDb.ListPrice = product.ListPrice;
+                objFromDb.Price = product.Price;
+                objFromDb.Price100 = product.Price100;
+                objFromDb.Price50 = product.Price50;
+                objFromDb.Title = product.Title;
+                objFromDb.CategoryId = product.CategoryId;
+                objFromDb.CoverTypeId = product.CoverTypeId;
+                objFromDb.Description = product.Description;
+                objFromDb.Author = product.Author;
             }
         }
     }
