821913bfac2fb763261ceac6b524a73ccd247996
Arun Kumar <arun.kumar@klm.com>
2020-03-03 13:15:41 +0100

    Add More Properties to Existing AspNetUsers Table

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+   public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        //This porperty will not be pushed to the database
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
